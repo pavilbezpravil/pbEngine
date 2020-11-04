4f31d02c2461531f32a196779963ff7bdb27ea3a
Pavel <pavilbezpravil@gmail.com>
2020-11-04 22:03:09 +0700

    add pbr shader

--- pbeEditor/assets/shaders/base.hlsl
+++ pbeEditor/assets/shaders/base.hlsl
@@ -1,8 +1,11 @@
+static const float PI = 3.14159265359;
+
 #ifdef COLOR_PASS
 	#define PS_OUT_COLOR0
 #endif
 
 #include "input_output.hlsli"
+#include "pbr.hlsli"
 
 cbuffer cbPass : register(b0) {
 	float4x4 gVP;
@@ -56,84 +59,45 @@ float GetShadowFactor(float3 posW, float3 normalW) {
 	return gShadowMap.Sample(gPointSampler, shadowMapUV) > pixelShadowMapDepth - bias;
 }
 
-static const float PI = 3.14159265359;
-
-float3 fresnelSchlick(float cosTheta, float3 F0) {
-	return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-
-float DistributionGGX(float3 N, float3 H, float roughness) {
-	float a = roughness * roughness;
-	float a2 = a * a;
-	float NdotH = max(dot(N, H), 0.0);
-	float NdotH2 = NdotH * NdotH;
-	float num = a2;
-	float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-	denom = PI * denom * denom;
-	return num / denom;
-}
-
-float GeometrySchlickGGX(float NdotV, float roughness) {
-	float r = (roughness + 1.0);
-	float k = (r * r) / 8.0;
-	float num = NdotV;
-	float denom = NdotV * (1.0 - k) + k;
-	return num / denom;
-}
-
-float GeometrySmith(float3 N, float3 V, float3 L, float roughness) {
-	float NdotV = max(dot(N, V), 0.0);
-	float NdotL = max(dot(N, L), 0.0);
-	float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-	float ggx1 = GeometrySchlickGGX(NdotL, roughness);
-	return ggx1 * ggx2;
-}
-
-
 PS_OUT mainPS(VS_OUT input) {
 	PS_OUT output;
 
 	#ifdef COLOR_PASS
-		float3 albedo = float3(177, 227, 199) / 256.f;
-		float metallic = 0;
-		float roughness = 0.3;
-		float ao = 0.1;
+		float3 posW = input.posW;
+		float3 normalW = normalize(input.normalW);
 
-		float3 WorldPos = input.posW;
+		float3 V = normalize(gCamPos - posW);
 
-		float3 N = normalize(input.normalW);
-		float3 V = normalize(gCamPos - WorldPos);
+		Surface surf;
+		surf.posW = posW;
+		surf.normalW = normalW;
+		surf.albedo = float3(177, 227, 199) / 256.f;
+		surf.metallic = 0;
+		surf.roughness = 0.1;
+
 		float3 F0 = 0.04;
-		F0 = lerp(F0, albedo, metallic);
+		F0 = lerp(F0, surf.albedo, surf.metallic);
+		surf.F0 = F0;
+		
 		// reflectance equation
 		float3 Lo = 0.0;
-
 		for(int i = 0; i < 1; ++i) {
 			// calculate per-light radiance
 			// float3 L = normalize(lightPositions[i] - WorldPos);
 			float3 L = normalize(-gDirection);
-			float3 H = normalize(V + L);
 			// float distance = length(lightPositions[i] - WorldPos);
 			// float attenuation = 1.0 / (distance * distance);
-			float attenuation = 1.0 * GetShadowFactor(input.posW, input.normalW);
-			// float3 radiance = lightColors[i] * attenuation;
+			float attenuation = 1.0 * GetShadowFactor(posW, normalW);
 			float3 radiance = gColor * attenuation;
-			// cook-torrance brdf
-			float NDF = DistributionGGX(N, H, roughness);
-			float G = GeometrySmith(N, V, L, roughness);
-			float3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
-			float3 kS = F;
-			float3 kD = float3(1, 1, 1) - kS;
-			kD *= 1.0 - metallic;
-			float3 numerator = NDF * G * F;
-			float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
-			float3 specular = numerator / max(denominator, 0.001);
-			// add to outgoing radiance Lo
-			float NdotL = max(dot(N, L), 0.0);
-			Lo += (kD * albedo / PI + specular) * radiance * NdotL;
+
+			Lo += SurfacePBRShade(surf, V, L, radiance);
 		}
-		float3 ambient = 0.03 * albedo * ao;
+
+		const float ao = 0.5;
+		float3 ambient = 0.03 * surf.albedo * ao;
+
 		float3 color = ambient + Lo;
+
 		color = color / (color + 1.0);
 		color = pow(color, 1.0 / 2.2);
 
---
+++ pbeEditor/assets/shaders/pbr.hlsli
@@ -0,0 +1,60 @@
+float3 fresnelSchlick(float cosTheta, float3 F0) {
+	return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
+}
+
+float DistributionGGX(float3 N, float3 H, float roughness) {
+	float a = roughness * roughness;
+	float a2 = a * a;
+	float NdotH = max(dot(N, H), 0.0);
+	float NdotH2 = NdotH * NdotH;
+	float num = a2;
+	float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+	denom = PI * denom * denom;
+	return num / denom;
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness) {
+	float r = (roughness + 1.0);
+	float k = (r * r) / 8.0;
+	float num = NdotV;
+	float denom = NdotV * (1.0 - k) + k;
+	return num / denom;
+}
+
+float GeometrySmith(float3 N, float3 V, float3 L, float roughness) {
+	float NdotV = max(dot(N, V), 0.0);
+	float NdotL = max(dot(N, L), 0.0);
+	float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+	float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+	return ggx1 * ggx2;
+}
+
+struct Surface {
+	float3 posW;
+	float3 normalW;
+	float3 albedo;
+	float roughness;
+	float metallic;
+	float3 F0;
+};
+
+float3 SurfacePBRShade(in Surface surf, float3 V, float3 L, float3 radiance) {
+	float3 H = normalize(V + L);
+
+	// cook-torrance brdf
+	float NDF = DistributionGGX(surf.normalW, H, surf.roughness);
+	float G = GeometrySmith(surf.normalW, V, L, surf.roughness);
+	float3 F = fresnelSchlick(max(dot(H, V), 0.0), surf.F0);
+
+	float3 kS = F;
+	float3 kD = float3(1, 1, 1) - kS;
+	kD *= 1.0 - surf.metallic;
+
+	float3 numerator = NDF * G * F;
+	float denominator = 4.0 * max(dot(surf.normalW, V), 0.0) * max(dot(surf.normalW, L), 0.0);
+	float3 specular = numerator / max(denominator, 0.001);
+
+	// add to outgoing radiance Lo
+	float NdotL = max(dot(surf.normalW, L), 0.0);
+	return (kD * surf.albedo / PI + specular) * radiance * NdotL;
+}
